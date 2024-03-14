9d111e4286c33995b8ec2ad0974c884e417da289
frederic siluvangi <105233528+fredsilu@users.noreply.github.com>
2024-03-14 19:52:39 +0000

    Viewadded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
+import Search from "./Components/Search";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
+    /* <div className="App">
+      //<h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen! Ceci est vrai</h2>
     </div>
+    */
+    <Search />
   );
 }
